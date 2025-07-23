460ed4ecda05cdfbd5f0194bca862ad41233bea9
arminas-bartkus <arminas.bartkus@gmail.com>
2025-07-23 23:04:18 +0100

    Create File Structure, Update basic CSS layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("greeting")
+import "./css/styles.css";
+import "./css/reset.css"
+
+
+class ToDoTask {
+    
+    constructor(taskTitle, taskDesc, dueDate,
+        priorityLvl /* which project to add to? */) {
+            this.taskTitle = taskTitle;
+            this.taskDesc = taskDesc;
+            this.dueDate = dueDate;
+            this.priorityLvl = priorityLvl;
+        }
+}
+
+class Project {
+    constructor(title) {
+        this.title = title;
+    }
+}
+
+class SubHeading {
+    constructor(title) {
+        this.title = title;
+    }
+}
