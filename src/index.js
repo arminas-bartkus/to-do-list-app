26e7d9efab78485a32d88d18c5e832a012489615
arminas-bartkus <arminas.bartkus@gmail.com>
2025-08-13 20:46:40 +0100

    Edit index.js

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ let editTaskModal;
 
     
         clearLocalStorageBtn.addEventListener("click", function() {
-            clearLocalStorage()
+            clearLocalStorage();
         });
 
         addNewProjectBtn.addEventListener("click", () => {
